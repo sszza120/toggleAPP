40a4e92cb1fa7dc15fce727913d396ebeb0fd56b
sszza120 <z875ab@gmail.com>
2020-05-19 14:04:37 +0800

    完成專案

--- app/src/main/java/com/sszza120/toggleapp/MainActivity.java
+++ app/src/main/java/com/sszza120/toggleapp/MainActivity.java
@@ -23,8 +23,10 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         img_bulb_on = findViewById(R.id.img_bulb_on);
         layout = findViewById(R.id.layout);
         toggleButton = findViewById(R.id.toggleButton);
+        //設定ID元件
 
         layout.setBackgroundColor(Color.parseColor("#FFFF37"));
+        //預設背景顏色為黃色
         toggleButton.setOnCheckedChangeListener(this);
     }
 
@@ -33,13 +35,17 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         if (b) {
             layout.setBackgroundColor(Color.parseColor("#000000"));
             img_bulb_off.setVisibility(View.VISIBLE);
+            //顯示img_bulb_off元件圖片
             img_bulb_on.setVisibility(View.INVISIBLE);
+            //隱藏img_bulb_on元件圖片
 
         }else {
 
             layout.setBackgroundColor(Color.parseColor("#FFFF37"));
             img_bulb_off.setVisibility(View.INVISIBLE);
+            //隱藏img_bulb_off元件圖片
             img_bulb_on.setVisibility(View.VISIBLE);
+            //顯示img_bulb_on元件圖片
         }
     }
 }
